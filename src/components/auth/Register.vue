fd99420dbcc4792cf379cbf8353943d99674c7c6
fuchsily <victoria.fuchs@arcor.de>
2024-05-19 11:21:47 +0200

    Validierungs Schema für Login mit Yup erstellt

--- src/components/auth/Register.vue
+++ src/components/auth/Register.vue
@@ -7,12 +7,12 @@
       <h2>Jetzt registrieren</h2>
       <p>
         oder
-        <a class="text-vue2" role="button"
-          >melden Sie sich mit Ihrem Konto an</a
-        >
+        <a class="text-vue2" role="button">
+          melden Sie sich mit Ihrem Konto an
+        </a>
       </p>
     </div>
-    <Form @submit="submitData">
+    <Form @submit="submitData" :validation-schema="schema" v-slot="{ errors }">
       <div class="form-row">
         <div class="form-group col-md-8 offset-2">
           <label for="email"><strong>E-Mail-Adresse</strong></label>
@@ -23,6 +23,7 @@
             class="form-control"
             id="email"
           />
+          <small class="text" v-if="errors.email">{{ errors.email }}</small>
         </div>
       </div>
       <div class="form-row">
@@ -35,6 +36,7 @@
             class="form-control"
             id="password"
           />
+          <small class="text" v-if="errors.password">{{ errors.password }}</small>
         </div>
       </div>
       <div class="form-row">
@@ -49,12 +51,13 @@
             class="form-control"
             id="confirmPassword"
           />
+          <small class="text" v-if="errors.confirmPassword">{{ errors.confirmPassword }}</small>
         </div>
       </div>
       <div class="form-row mt-3">
         <div class="form-group col-md-8 offset-2">
           <div class="d-grid">
-            <button class="btn bg-vue">Registrieren</button>
+            <button class="btn bg-vue" type="submit">Registrieren</button>
           </div>
         </div>
       </div>
@@ -64,12 +67,34 @@
 
 <script>
 import { Form, Field } from "vee-validate";
+import * as yup from "yup";
 
 export default {
   name: "Register",
   components: {
     Form,
     Field,
+  },
+  data() {
+    const schema = yup.object().shape({
+      email: yup
+        .string()
+        .email("Gültige E-Mail Adresse ist erforderlich")
+        .required("E-Mail Adresse wird benötigt")
+        .trim()
+        .email("Das ist keine gülitige Email Adresse"),
+      password: yup
+        .string()
+        .required("Passwort wird benötigt")
+        .min(6, "Das Passwort muss min. 6 Zeichen lang sein"),
+      confirmPassword: yup
+        .string()
+        .oneOf([yup.ref("password"), null], "Passwörter müssen übereinstimmen")
+        .required("Passwort Wiederholung ist erforderlich"),
+    });
+    return {
+      schema,
+    };
   },
   methods: {
     submitData(values) {
